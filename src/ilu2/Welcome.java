af25ad8fe8dbbc9a936de76e79cd475700aa5e32
unknown <bermobiz32@gmail.com>
2023-04-21 16:21:35 +0200

    Ex_5

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -11,7 +11,7 @@ public class Welcome {
 				return cris(input);
 			}
 			if (input.contains(",")) {
-				resultat.append(deuxNoms(input));
+				resultat.append(plusieursNoms(input));
 			}else {
 				resultat.append(premiereLettreMajuscule(input));
 			}
@@ -30,19 +30,21 @@ public class Welcome {
 	
 	private static String cris(String input) {
 		StringBuilder resultat = new StringBuilder("HELLO, ");
-		return resultat.append(input).append(" !").toString();
+		return resultat.append(justePrenom(input)).append(" !").toString();
 	}
 	
 	private static String justePrenom(String input) {
 		return input.trim();
 	}
 	
-	private static String deuxNoms(String input) {
+	private static String plusieursNoms(String input) {
 		StringBuilder resultat = new StringBuilder();
 		String[] intermediaire = input.split(",");
-		resultat.append(premiereLettreMajuscule(justePrenom(intermediaire[0])));
-		resultat.append(", ");
-		resultat.append(premiereLettreMajuscule(justePrenom(intermediaire[1])));
+		for (int i=0;i<intermediaire.length;i++) {
+			resultat.append(premiereLettreMajuscule(justePrenom(intermediaire[i])));
+			resultat.append(", ");
+		}
+		resultat.delete(resultat.length()-2, resultat.length());
 		return resultat.toString();
 	}
 
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -28,7 +28,7 @@ class WelcomeTest {
 	@Test
 	void Ex_3() {
 		assertEquals("HELLO, JERRY !",Welcome.Welcome("JERRY"));
-		assertEquals("HELLO, FOOL !",Welcome.Welcome("FOOL"));
+		assertEquals("HELLO, FOOL !",Welcome.Welcome("  FOOL"));
 	}
 	
 	@Test
@@ -41,6 +41,19 @@ class WelcomeTest {
 		assertEquals("Hello, Amy, Bob",Welcome.Welcome("amy,Bob"));
 		assertEquals("Hello, Amy, Bob",Welcome.Welcome("Amy, bob"));
 		assertEquals("Hello, Amy, Bob",Welcome.Welcome("Amy,bob"));
+	}
+	
+	@Test
+	void Ex_5() {
+		assertEquals("Hello, Amy, Bob, Jerry",Welcome.Welcome("amy,bob,jerry"));
+		assertEquals("Hello, Amy, Bob, Jerry, Sylvestre, Tom, Cassandra",Welcome.Welcome("amy,bob,jerry,Sylvestre,tom,cassandra"));
+		assertEquals("Hello, Amy, Bob, Jerry",Welcome.Welcome("amy, bob, Jerry"));
+		assertEquals("Hello, Amy, Bob, Jerry",Welcome.Welcome("Amy,Bob,Jerry"));
+		assertEquals("Hello, Amy, Bob, Jerry",Welcome.Welcome("Amy, Bob,  Jerry"));
+		assertEquals("Hello, Amy, Bob, Jerry",Welcome.Welcome("amy, Bob,Jerry"));
+		assertEquals("Hello, Amy, Bob, Jerry",Welcome.Welcome("amy,Bob,jerry"));
+		assertEquals("Hello, Amy, Bob, Jerry",Welcome.Welcome("Amy, bob,jerry"));
+		assertEquals("Hello, Amy, Bob, Jerry",Welcome.Welcome("Amy,bob,Jerry"));
 	}
 
 }
